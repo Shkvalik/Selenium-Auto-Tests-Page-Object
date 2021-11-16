5d31676411202bb9d425045e77b15e894248a010
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-17 01:17:54 +0200

    Add new tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,5 @@
-import time
-
 import pytest
-
 from pages.product_page import ProductPage
-
 
 
 def test_product_page(browser):
@@ -31,6 +27,44 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_goods_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_correct_confirming_message()
-    time.sleep(10)
     page.should_be_correct_message_about_basket_price()
 
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_goods_to_basket()
+    page.solve_quiz_and_get_code()
+    page.confirming_message_should_not_be()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.confirming_message_should_not_be()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_goods_to_basket()
+    page.solve_quiz_and_get_code()
+    page.confirming_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
