160dfd29b6d3e586cac6b69dda07e4822c1f13e5
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-17 22:07:25 +0200

    Add locator for login page

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BTN_BASKET = (By.CSS_SELECTOR, 'a[class="btn btn-default"]')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
@@ -28,8 +29,18 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
-    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    PART_URL = "login"
+
+    LOGIN_FORM = (By.ID, "login_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, 'input[name="login-username"]')
+    LOGIN_PASS = (By.CSS_SELECTOR, 'input[name="login-password"]')
+    LOGIN_BTN = (By.CSS_SELECTOR, 'button[name="login_submit"]')
+
+    REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, 'input[name="registration-email"]')
+    REGISTER_PASS1 = (By.CSS_SELECTOR, 'input[name="registration-password1"]')
+    REGISTER_PASS2 = (By.CSS_SELECTOR, 'input[name="registration-password2"]')
+    REGISTER_BTN = (By.CSS_SELECTOR, 'button[name="registration_submit"]')
 
 
 class ProductPageLocators():
