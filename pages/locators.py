b9ce71d38fb98d9bcbfa41ab951e6ec081d0b47f
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-17 19:10:30 +0200

    Add locators for basket page

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,23 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BTN_BASKET = (By.CSS_SELECTOR, 'a[class="btn btn-default"]')
+
+
+class BasketPageLocators():
+    empty_text = {
+        "en-gb": "Your basket is empty",
+        "en": "Your basket is empty",
+        "ru": "Ваша корзина пуста",
+        "es": "Tu carrito esta vacío",
+        "fr": "Votre panier est vide",
+        "de": "Ihr Warenkorb ist leer",
+        "pl": "Twój koszyk jest pusty"
+    }
+    BASKET_INNER = (By.CSS_SELECTOR, "#content_inner p")
+    # если в корзине есть товар то появляются:
+    BASKET_TITLE = (By.CSS_SELECTOR, "#content_inner div.basket-title")
+    BASKET_ROW = (By.CSS_SELECTOR, "#content_inner div.basket-title div.row")
 
 
 class MainPageLocators():
