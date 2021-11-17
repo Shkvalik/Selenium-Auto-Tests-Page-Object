a0dd53cc1cd471a2b841d352cf86443713c7579f
Shkvalik <wal2004yandex.ru@gmail.com>
2021-11-17 19:12:26 +0200

    Add test for basket page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 import pytest
+
+from pages.basket_page import BasketPage
 from pages.product_page import ProductPage
 
 
@@ -62,9 +64,23 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-def test_guest_can_go_to_login_page_from_product_page (browser):
+def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
     page.go_to_login_page()
+
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_btn_basket()
+    page.go_to_basket_page()
+
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+
+
